3004f736d6c535e5213bb9abe5903f4f73caa81c
79686 <mrak5455@yandex.ru>
2024-08-15 16:22:45 +0300

    исправил сушность переопрделил equals and hashCode

--- src/main/java/aleksey/wallet/model/Wallet.java
+++ src/main/java/aleksey/wallet/model/Wallet.java
@@ -1,14 +1,16 @@
 package aleksey.wallet.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Entity
 @Builder
 @Table(name = "wallet")
@@ -21,4 +23,20 @@ public class Wallet {
     private UUID uuid;
     @Column(name = "balance")
     private Long balance;
+
+    @Override
+    public final boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null) return false;
+        Class<?> oEffectiveClass = object instanceof HibernateProxy ? ((HibernateProxy) object).getHibernateLazyInitializer().getPersistentClass() : object.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Wallet wallet = (Wallet) object;
+        return getUuid() != null && Objects.equals(getUuid(), wallet.getUuid());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
