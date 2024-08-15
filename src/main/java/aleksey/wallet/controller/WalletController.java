275944a8b48e0e44b672500d3ffd26821efdab89
79686 <mrak5455@yandex.ru>
2024-08-15 19:20:28 +0300

    сделал интерфайсы

--- src/main/java/aleksey/wallet/controller/WalletController.java
+++ src/main/java/aleksey/wallet/controller/WalletController.java
@@ -1,38 +1,19 @@
 package aleksey.wallet.controller;
 
-
 import aleksey.wallet.dto.WalletRequest;
 import aleksey.wallet.dto.WalletResponse;
-import aleksey.wallet.service.WalletService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.UUID;
 
-@Slf4j
-@RestController
-@RequiredArgsConstructor
-@RequestMapping("/api/v1")
-public class WalletController {
-
-    private final WalletService walletService;
-
+public interface WalletController {
     @PostMapping("/wallet")
-    public void updateBalance(@RequestBody @Valid WalletRequest request) {
-        log.info("Пришел POST запрос api/v1/wallet с телом: {}", request);
-        walletService.updateBalance(request);
-        log.info("Отправлен ответ 200 без тела");
-    }
+    void updateBalance(@RequestBody @Valid WalletRequest request);
 
     @GetMapping("/wallets/{uuid}")
-    public WalletResponse getBalance(@PathVariable UUID uuid) {
-        log.info("Пришел GET запрос /api/v1/wallets/{}", uuid);
-        final WalletResponse response = walletService.getBalance(uuid);
-        log.info("Отправлен ответ для GET запроса /api/v1/wallets/{} с телом: {}", uuid, response);
-        return response;
-    }
-
-
+    WalletResponse getBalance(@PathVariable UUID uuid);
 }
